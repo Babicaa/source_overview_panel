e32f77af299e930aaf43d6725440978819b5abce
uzonwanne_c <christiana.uzonwanne@psi.ch>
2018-10-17 10:23:15 +0200

    handling errors (page not found)

--- overview_panel/server.py
+++ overview_panel/server.py
@@ -99,6 +99,9 @@ class ClientThread(Thread):
     def run(self):
         self.receive_stream()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__== "__main__":
     socketio.run(app)
