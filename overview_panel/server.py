3814a05b5415d8c97e7b57392659d227860ec800
uzonwanne_c <christiana.uzonwanne@psi.ch>
2018-11-06 11:45:15 +0100

    Defined store in render_template function

--- overview_panel/server.py
+++ overview_panel/server.py
@@ -1,5 +1,7 @@
 
 # Start with a basic flask app webpage.
+from string import Template
+
 from flask_socketio import SocketIO, emit
 from flask_restful import Resource, Api
 from flask_material import Material
@@ -12,36 +14,54 @@ from matplotlib import pyplot, image
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
+
 # App config.
 DEBUG = True
+
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config['SECRET_KEY'] = 'christy'
 
 socketio = SocketIO(app)
+
+global store
+store = {}
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 
 
 
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
 
+@app.route("/addchannel", methods=['GET', 'POST'])
+def form_data():
 
-@app.route("/", methods=['GET', 'POST'])
-def hello():
+    global store
     form = ReusableForm(request.form)
-
     print(form.errors)
+
     if request.method == 'POST':
         name = request.form['name']
-        print(name)
+        store = {'key': name}
+        print(store)
+
 
         if form.validate():
-            # Save the comment here.
+
             flash('Channel successfully added! ')
         else:
             flash('Error: All the form fields are required. ')
 
-    return render_template('addchannel.html', form=form)
+    return render_template('addchannel.html', form=form, store=store)
+
+
+
+
+
 
 
 if __name__ == "__main__":
